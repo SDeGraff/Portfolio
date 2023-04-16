fc319ecd1c8071f33999ccd30bda664c9ff25ddd
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2023-04-16 08:58:17 -0500

    Landing page

---
+++ src/scenes/Footer.jsx
@@ -0,0 +1,22 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
+
+//footer of portfolio that shows my social media links
+const Footer = () => {
+  return (
+    <footer className="h-64 bg-red pt-10">
+      <div className="w-10/12 mx-auto">
+        <SocialMediaIcons />
+        <div className="md:flex justify-center md:justify-between text-center ">
+          <p className="font-playfair font-semibold text-2xl text-yellow">
+            JANE ESPER
+          </p>
+          <p className="font-playfair text-md text-yellow">
+            ©2022 ESPER. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
